4fc3cc1ab3b4ad0ee7ab3ad4079aff0ce2d0690b
Edanriell <Edanriell@gmail.com>
2024-07-01 20:14:25 +0300

    decomposed price plan

---
+++ Client/src/widgets/price-plan/price-plan.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+	selector: "price-plan",
+	standalone: true,
+	templateUrl: "./price-plan.html",
+	styleUrl: "./price-plan.less"
+})
+export class PricePlan {}
