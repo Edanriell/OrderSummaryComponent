ba077f129fbacfbe6cb84da144b490095cabd965
Edanriell <Edanriell@gmail.com>
2024-07-02 15:17:40 +0300

    finished link component

--- Client/src/widgets/order-summary/order-summary.ts
+++ Client/src/widgets/order-summary/order-summary.ts
@@ -1,11 +1,13 @@
 import { Component } from "@angular/core";
+
 import { PricePlan } from "@widgets/price-plan";
+import { CustomLink } from "@shared/ui";
 
 @Component({
 	selector: "order-summary",
 	standalone: true,
 	templateUrl: "./order-summary.html",
-	imports: [PricePlan],
+	imports: [PricePlan, CustomLink],
 	styleUrl: "./order-summary.less"
 })
 export class OrderSummary {}
